e97f51a39b14fa8cc25ea18ac6a06e859769bb65
JefersonLC <c.jeferson.lc@gmail.com>
2023-07-06 18:35:48 -0500

    updated TooManyMatches

--- src/components/TooManyMatches.tsx
+++ src/components/TooManyMatches.tsx
@@ -1,3 +1,12 @@
+import { divStyle, p1Style, p2SpanStyle, p2Style } from '../css/TooManyMatches'
+
 export default function TooManyMatches() {
-  return <p>Muchas coincidencias, sea más específico</p>
+  return (
+    <div className={divStyle}>
+      <p className={p1Style}>There are too many matches</p>
+      <p className={p2Style}>
+        Please be more specific <span className={p2SpanStyle}>;)</span>
+      </p>
+    </div>
+  )
 }
