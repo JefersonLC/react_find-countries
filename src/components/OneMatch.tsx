c76c276d6a5474077014bd805aff06cb4ad92323
JefersonLC <c.jeferson.lc@gmail.com>
2023-07-06 18:35:59 -0500

    updated OneMatch

--- src/components/OneMatch.tsx
+++ src/components/OneMatch.tsx
@@ -1,5 +1,18 @@
-import { Country } from '../services/api.d'
+import useSWR from 'swr'
+import { getData } from '../services/api'
+import { Country, CountryInfo } from '../services/api.d'
 
 export default function OneMatch({ country }: { country: Country }) {
+  const API_WEATHER_URL = 'http://api.weatherstack.com/current'
+  const key: string = import.meta.env.VITE_API_KEY
+  const name = country.name.common
+
+  const { data, error, isLoading } = useSWR<CountryInfo>(
+    `${API_WEATHER_URL}?access_key=${key}&query=${name}`,
+    getData
+  )
+
+  console.log(data)
+
   return <div>{country.flag}</div>
 }
