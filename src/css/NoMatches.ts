4e9994a479e3b437cf42bc4a710977c03faea9b7
JefersonLC <c.jeferson.lc@gmail.com>
2023-07-06 18:35:35 -0500

    updated styles

---
+++ src/css/NoMatches.ts
@@ -0,0 +1,22 @@
+import { css } from '../../styled-system/css'
+
+export const divStyle = css({
+  mt: '8',
+  textAlign: 'center',
+  fontSize: '1.65rem',
+})
+
+export const p1Style = css({
+  fontWeight: 'bold',
+  wordSpacing: '4px',
+})
+
+export const p1SpanStyle = css({
+  color: 'yellow.400',
+})
+
+export const spanStyle = css({
+  fontSize: 'xl',
+  fontFamily: 'mono',
+  color: 'orange.500',
+})
---
+++ src/css/TooManyMatches.ts
@@ -0,0 +1,20 @@
+import { css } from '../../styled-system/css'
+
+export const divStyle = css({
+  mt: '8',
+  textAlign: 'center',
+  fontSize: '1.65rem',
+})
+
+export const p1Style = css({
+  fontWeight: 'bold',
+  wordSpacing: '4px',
+})
+
+export const p2Style = css({
+  fontSize: '2xl',
+})
+
+export const p2SpanStyle = css({
+  color: 'orange.500',
+})
