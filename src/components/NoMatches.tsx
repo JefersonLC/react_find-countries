adbeb0c88b988a0b617bd337b1a2299669b0ea16
JefersonLC <c.jeferson.lc@gmail.com>
2023-07-06 18:36:08 -0500

    updated NoMatches

--- src/components/NoMatches.tsx
+++ src/components/NoMatches.tsx
@@ -1,5 +1,17 @@
+import {
+  divStyle,
+  p1SpanStyle,
+  p1Style,
+  spanStyle
+} from '../css/NoMatches'
+
 export default function NoMatches() {
   return (
-    <div>No matches</div>
+    <div className={divStyle}>
+      <p className={p1Style}>
+        No matches found <span className={p1SpanStyle}>:(</span>
+      </p>
+      <span className={spanStyle}>404</span>
+    </div>
   )
 }
